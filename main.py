58d9c8bc34adc9f0272d311816cd84beead65981
Joaquin Araujo <joaquin.araujo@abstracta.com.uy>
2025-08-18 13:46:40 -0300

    Token provisioning and logging configuration
    As well as tools (one at this point)

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-from server import mcp
+import os
+import sys
+import logging
+from server import register_tools
+from mcp.server.fastmcp import FastMCP
+from config.token import BzmToken, BzmTokenError
+
+BLAZEMETER_API_KEY_FILE_PATH = os.getenv('BLAZEMETER_API_KEY')
+logging.basicConfig(level=logging.ERROR)
+
+def run():
+    try:
+        token = BzmToken.from_file(BLAZEMETER_API_KEY_FILE_PATH)
+    except BzmTokenError as e:
+        logging.error(f"Error loading BlazeMeter token: {e}")
+        raise SystemExit(1)
+
+    mcp = FastMCP("bzm-mcp")
+    register_tools(mcp, token)
+    mcp.run(transport="stdio")
 
 if __name__ == "__main__":
-    mcp.run()
+    run()
