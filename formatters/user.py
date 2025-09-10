d6512361b2c4a89872197406af2f6cca76cf3b28
David <3dgiordano@gmail.com>
2025-09-10 14:55:23 -0300

    MOB-42944: Custom formatter for User

---
+++ formatters/user.py
@@ -0,0 +1,26 @@
+from typing import List, Any, Dict
+
+from tools.utils import get_date_time_iso
+
+
+def format_users(users: List[Any]) -> List[Dict[str, Any]]:
+    formatted_users = []
+    for user in users:
+        user_element = {
+            "user_id": user.get("id"),
+            "display_name": user.get("displayName"),
+            "first_name": user.get('firstName'),
+            "last_name": user.get('lastName'),
+            "email": user.get("email"),
+            "access": get_date_time_iso(user.get("access")),
+            "login": get_date_time_iso(user.get("login")),
+            "created": get_date_time_iso(user.get("created")),
+            "updated": get_date_time_iso(user.get("updated")),
+            "time_zone": user.get("timezone", 0),
+            "enabled": user.get("enabled"),
+            "ai_consent": user.get("aiConsent"),
+            "default_project_id": user.get("defaultProjectId"),
+        }
+        formatted_users.append(user_element)
+
+    return formatted_users
